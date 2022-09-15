f9fdce0fa14b531d7b391adf32a06d5a6e026ad3
Wilian Pereira <wilian_pereira92@outlook.com>
2022-09-15 20:59:30 -0300

    feat: implementado App.tsx

---
+++ App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Home from './src/screen/Home';
+import { ThemeProvider } from 'styled-components/native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import light from './src/theme/light';
+import Splash from './src/screen/Splash';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <ThemeProvider theme={light}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Splash">
+          <Stack.Screen
+            name="Splash"
+            component={Splash}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{
+              headerShown: false,
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ThemeProvider>
+  );
+}
